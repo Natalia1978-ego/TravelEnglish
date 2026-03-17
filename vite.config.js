2208809133e0e8db99a1b5ff283f691ef6dece6e
Natalia <nikulenkovan@mail.ru>
2026-03-17 17:14:50 +0300

    Build and prepare for GH Pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-// Простой конфиг без плагинов
-export default defineConfig({
-root: '.',
-base: '/TravelEnglish/', // ВАЖНО: имя репозитория c учётом регистра
-});
